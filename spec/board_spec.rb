ac389db2a2ac4f947d8e84e6330b3efae034aa4f
ontruster74 <ontruster74@gmail.com>
2024-12-20 16:03:05 -0700

    add render method test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -74,6 +74,10 @@ describe Board do
   end
 
   describe '#render' do
-    
+    it 'can render cells as a visible board' do
+      @board.place(@cruiser, ["A1", "A2", "A3"])
+      expect(@board.render).to eq("  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n")
+      expect(@board.render(true)).to eq("  1 2 3 4 \nA S S S . \nB . . . . \nC . . . . \nD . . . . \n")
+    end
   end
 end
