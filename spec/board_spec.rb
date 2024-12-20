c718079e2c581e2d62e2a46bec380384146807f4
Sebastian McKee <sebmcke@gmail.com>
2024-12-20 15:16:44 -0600

    Add correct placement test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -51,5 +51,10 @@ describe Board do
       expect(@board.valid_placement?(@cruiser, ["A1", "B2", "C3"]).to eq(false))
       expect(@board.valid_placement?(@submarine, ["C2", "D3"]).to eq(false))
     end
+
+    it 'can validate correct placements' do
+      expect(@board.valid_placement?(@submarine, ["A1", "A2"]).to eq(true))
+      expect(@board.valid_placement?(@cruiser, ["B1", "C1", "D1"]).to eq(true))
+    end
   end
 end
