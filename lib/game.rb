b819b2a62a2c988ed327e4556f6919d6369503a7
ontruster74 <ontruster74@gmail.com>
2024-12-22 15:56:52 -0700

    add turn method funcctionality

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,11 @@
 class Game 
+  attr_reader :computer, :player_board
+
+  def initialize
+    @computer = Computer.new(Board.new)
+    @player_board = Board.new
+  end
+
   def intro
     puts "--WELCOME TO BATTLESHIP--"
     puts "Enter p to play or q to quit"
@@ -14,11 +21,22 @@ class Game
 
   def gameplay
     while true do
-      
+      self.turn
+      break if @computer.lost_game || @player_board.lost_game
     end
   end
 
   def turn
+    puts "==========PLAYER BOARD=========="
+    puts @player_board.render
+    puts "==========COMPUTER BOARD=========="
+    puts @computer.board.render
+    target = @computer.fire_upon(@player_board).coordinate
+    puts "The Computer fires on #{target}!"
+    puts "Enter a coordinate to fire upon:"
+    while true do
+      
+    end
     
   end
 
