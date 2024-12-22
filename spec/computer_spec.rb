ea068a451e26b7c38315bf2809df7ac93b56d186
Sebastian McKee <sebmcke@gmail.com>
2024-12-21 18:44:19 -0600

    Add place_ship test

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -6,6 +6,7 @@ require './lib/cell'
 
 describe Computer do
     before do
+        @cruiser = Ship.new("Cruiser", 3)
         @board = Board.new
         @computer = Computer.new(@board)
     end
@@ -17,6 +18,18 @@ describe Computer do
 
         it 'has a board' do
             expect(@computer.board).to eq(@board)
+        end
+    end
+
+    describe "#place_ship" do
+        it 'can place ships' do
+            coordinates = @computer.place_ship(@cruiser)
+
+            expect(@board.valid_placement?(@cruiser, coordinates)).to eq(true)
+            
+            coordinates.each do |coordinate|
+                expect(@board.cells[coordinate].empty?).to eq(false)
+            end
         end
     end
 end
